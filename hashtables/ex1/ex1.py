c1c6ba55fe16160520ba8cf883583747439f6367
Ryan Ansberry <ansman92@gmail.com>
2020-04-17 18:48:16 -0700

    hashtables

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,16 +8,28 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
-
-    """
-    YOUR CODE HERE
-    """
-
-    return None
+    answer = []
+    # store each weight in the input list as keys
+    keys = weights
+    # store each weight's list index as its value
+    for i in range(len(keys)):
+        hash_table_insert(ht, keys[i], i)
+    # check to see if the hash table contains an entry for `limit - weight`
+    for i in range(len(keys)):
+        if hash_table_retrieve(ht, limit - keys[i]):
+        # If it does, then we've found the two items whose weights sum up to the `limit`!
+            answer.append(i)
+        else:
+            return None
+    print_answer(answer)
 
 
 def print_answer(answer):
     if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
+        print(str(answer[0] + "" + answer[1]))
     else:
         print("None")
+
+weights = [ 4, 6, 10, 15, 16 ]
+get_indices_of_item_weights(weights, 5, 21)
+# Should be "3 1"
